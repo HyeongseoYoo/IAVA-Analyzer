34c6089a4c5dd7a21cbad6016581c344a9f02202
HyeongseoYoo <hyeongseo0918@gmail.com>
2026-05-21 17:40:34 +0900

    Move concrete interpreter

--- lib/analyzer.ml
+++ lib/analyzer.ml
@@ -1,8 +1,7 @@
 open Syntax
 open Domain
 open Abs_dom
-
-type conf = { env : Env.t; mem : Mem.t; imode : Interrupt.t }
+open Interp
 
 type abs_conf = {
   asem : Abs_Sem.t;
@@ -11,7 +10,6 @@ type abs_conf = {
   errs : ErrorSet.t;
 }
 
-type result = { value : Value.t; pp : ProgramPoint.t; out : Outcome.t }
 type abs_res = { avalue : Abs_Val.t; app : PPSet.t }
 
 (* A single memory-modifying effect compiled from a handler body *)
@@ -57,11 +55,6 @@ type compiled_fixpoint = {
   fp_arrays  : fp_array_write list;
 }
 
-exception Runtime_error of string
-
-let var_tbl : VarTbl.t ref = ref VarTbl.empty
-let iset : IidSet.t ref = ref IidSet.empty
-let handlers : HandlerStore.t ref = ref HandlerStore.empty
 let size_tbl : Itv.t LblMap.t ref = ref LblMap.empty
 let handler_summaries : handler_summary HandlerStore.IidMap.t ref =
   ref HandlerStore.IidMap.empty
@@ -72,181 +65,6 @@ let use_compiled_fp : bool ref = ref false
 (* Forward reference: will be set to apply_fixpoint_to_conf after it is defined *)
 let post_steps_fn : (abs_conf -> abs_conf) ref = ref (fun c -> c)
 let widen_cnt = 3
-
-let rec eval ?(lvalue = false) (c : conf) (lbl_exp : Exp.lbl_t) : result * conf
-    =
-  let ({ lbl; exp; _ } : Exp.lbl_t) = lbl_exp in
-  let ({ env = _; mem; imode = _ } : conf) = c in
-  (* TODO: Done -> Non-Deterministic *)
-  let r = { value = Value.Unit; pp = Unit; out = Outcome.Done } in
-  (* TEST CODE *)
-  (* let r = (if lbl = Exp.Lbl.Main 3 then { value = Value.Unit; pp = Unit; out = Outcome.I 0 } else { value = Value.Unit; pp = Unit; out = Outcome.Done }) in  *)
-  let exp_r, exp_c =
-    match exp with
-    | Unit -> (r, c)
-    | Int n -> ({ r with value = Value.Int n }, c)
-    | Var x -> (
-        let l = Loc.get x in
-        if lvalue then ({ r with value = Value.Loc l }, c)
-        else
-          match Loc.Map.find_opt l mem with
-          | Some (v, p) -> ({ r with value = v; pp = p }, c)
-          | None ->
-              raise
-                (Runtime_error
-                   ("[Mem] Location " ^ Loc.string_of_t l ^ " not found")))
-    | Enable -> (r, { c with imode = Interrupt.Enabled })
-    | Disable -> (r, { c with imode = Interrupt.Disabled })
-    | Malloc (e1, e2) ->
-        let r1, c1 = eval c e1 in
-        let r2, c2 = eval c1 e2 in
-        let n =
-          match r1.value with
-          | Value.Int n' -> n'
-          | _ -> failwith "Malloc size must be an integer"
-        in
-        let v = r2.value in
-        let new_r = (v, ProgramPoint.Label lbl) in
-        let mem' =
-          List.init n (fun i -> Loc.alloc lbl i)
-          |> List.fold_left (fun m a -> Loc.Map.add a new_r m) c2.mem
-        in
-        ({ r with value = Value.Loc (Loc.alloc lbl 0) }, { c2 with mem = mem' })
-    | Deref (e1, e2) -> (
-        let r1, c1 = eval c e1 in
-        let r2, c2 = eval c1 e2 in
-        let base =
-          match r1.value with
-          | Value.Loc l -> l
-          | _ -> failwith "Deref base must be a location"
-        in
-        let offset =
-          match r2.value with
-          | Value.Int i -> i
-          | _ -> failwith "Deref offset must be an integer"
-        in
-        let l =
-          match base with
-          | Loc.VarLoc { id; offset = off } ->
-              Loc.VarLoc { id; offset = off + offset }
-          | Loc.HeapLoc { lbl; offset = off } ->
-              Loc.HeapLoc { lbl; offset = off + offset }
-        in
-        if lvalue then ({ r with value = Value.Loc l }, c2)
-        else
-          match Loc.Map.find_opt l c2.mem with
-          | Some (v, p) -> ({ r with value = v; pp = p }, c2)
-          | None ->
-              raise
-                (Runtime_error
-                   ("[Mem] Location " ^ Loc.string_of_t l ^ " not found")))
-    | Bop (bop, e1, e2) -> (
-        let r1, c1 = eval c e1 in
-        let r2, c2 = eval c1 e2 in
-        match bop with
-        | Eq ->
-            if Value.compare r1.value r2.value = 0 then
-              ({ r with value = Value.Int 1 }, c2)
-            else ({ r with value = Value.Int 0 }, c2)
-        | Lt -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ({ r with value = Value.Int (if i1 < i2 then 1 else 0) }, c2)
-            | _ -> failwith "Undefined operation")
-        | Gt -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ({ r with value = Value.Int (if i1 > i2 then 1 else 0) }, c2)
-            | _ -> failwith "Undefined operation")
-        | Ne ->
-            if Value.compare r1.value r2.value <> 0 then
-              ({ r with value = Value.Int 1 }, c2)
-            else ({ r with value = Value.Int 0 }, c2)
-        | Le -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ({ r with value = Value.Int (if i1 <= i2 then 1 else 0) }, c2)
-            | _ -> failwith "Undefined operation")
-        | Ge -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ({ r with value = Value.Int (if i1 >= i2 then 1 else 0) }, c2)
-            | _ -> failwith "Undefined operation")
-        | Plus -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                let res = i1 + i2 in
-                ({ r with value = Value.Int res }, c2)
-            | _ -> failwith "Undefined operation")
-        | Minus -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ({ r with value = Value.Int (i1 - i2) }, c2)
-            | _ -> failwith "Undefined operation")
-        | Times -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ({ r with value = Value.Int (i1 * i2) }, c2)
-            | _ -> failwith "Undefined operation")
-        | And -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ( {
-                    r with
-                    value = Value.Int (if i1 <> 0 && i2 <> 0 then 1 else 0);
-                  },
-                  c2 )
-            | _ -> failwith "Undefined operation")
-        | Or -> (
-            match (r1.value, r2.value) with
-            | Value.Int i1, Value.Int i2 ->
-                ( {
-                    r with
-                    value = Value.Int (if i1 <> 0 || i2 <> 0 then 1 else 0);
-                  },
-                  c2 )
-            | _ -> failwith "Undefined operation"))
-    | Assign (e1, e2) ->
-        let r1, c1 = eval c e1 ~lvalue:true in
-        let r2, c2 = eval c1 e2 in
-        let l =
-          match r1.value with
-          | Value.Loc l -> l
-          | _ -> failwith "Left-hand side of assignment must be a location"
-        in
-        let mem' = Loc.Map.add l (r2.value, ProgramPoint.Label lbl) c2.mem in
-        (r, { c2 with mem = mem' })
-    | Seq (e1, e2) ->
-        let _, c1 = eval c e1 in
-        eval c1 e2
-    | If (e1, e2, e3) -> (
-        let r1, c1 = eval c e1 in
-        match r1.value with
-        | Value.Int n -> if n <> 0 then eval c1 e2 else eval c1 e3
-        | _ -> failwith "Condition expression must evaluate to an integer")
-    | While (_, e1, e2) -> (
-        let r1, c1 = eval c e1 in
-        match r1.value with
-        | Value.Int n ->
-            if n <> 0 then
-              let _, c2 = eval c1 e2 in
-              eval c2 lbl_exp
-            else (r, c1)
-        | _ -> failwith "Condition expression must evaluate to an integer")
-  in
-  match exp_r.out with
-  | Outcome.Done -> (exp_r, exp_c)
-  | Outcome.I iid -> (
-      match HandlerStore.lookup !handlers iid with
-      (* TODO: Done -> Non-Deterministic *)
-      | None ->
-          ({ exp_r with out = Outcome.Done }, exp_c)
-          (* No handler, treat as Done *)
-      | Some exp ->
-          let _hdl_r, hdl_c =
-            eval { exp_c with imode = Interrupt.Disabled } exp
-          in
-          ({ exp_r with out = Outcome.Done }, { exp_c with mem = hdl_c.mem }))
 
 (* Helper *)
 let join_res r1 r2 =
@@ -1236,30 +1054,6 @@ let rec compile_handler (lbl_exp : Exp.lbl_t) : handler_summary =
       Compiled []
   | While _ | Malloc _ ->
       Fallback lbl_exp
-
-let init_conf (pgm : Program.t) : conf =
-  let c0 = { env = Env.empty; mem = Mem.empty; imode = Interrupt.Enabled } in
-  let _, c_globals = eval c0 pgm.global in
-  let hs', iset' =
-    List.fold_left
-      (fun (hs, iset) (d : Handler.t) ->
-        let hs = HandlerStore.add hs d.iid d.body in
-        let iset = IidSet.add (Handler.get_iid d) iset in
-        (hs, iset))
-      (HandlerStore.empty, IidSet.empty)
-      pgm.handler
-  in
-  iset := iset';
-  handlers := hs';
-  { env = c_globals.env; mem = c_globals.mem; imode = c_globals.imode }
-
-let def_intp (pgm : Program.t) : Mem.t =
-  let c_init = init_conf pgm in
-  print_endline "=== Initial Memory ===";
-  print_endline (Mem.string_of_t c_init.mem);
-  print_endline "=== Final Memory ===";
-  let _, c_final = eval c_init pgm.main in
-  c_final.mem
 
 let init_confa (pgm : Program.t) : abs_conf =
   let c0 =
